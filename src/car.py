641f867000bb6f3986fa520a5cd9660d9a60def9
LeoZotos <zotos.leo@gmail.com>
2023-05-01 17:38:47 +0200

    added functions to deal with winning or losing an auction

--- src/car.py
+++ src/car.py
@@ -15,12 +15,13 @@ class Car:
         # Rush factor is random between 0 and 1
         self.rush_factor = random.random()
         self.submitted_bid = 0
+        self.waiting_time = 0
 
     def __str__(self):
         return f'Car(id={self.id}), heading {self.direction}'
 
     def submit_bid(self):
-        # For now, randomly submit a bid.
+        # For now, randomly submit a bid. TODO: Incorporate rush factor
         self.submitted_bid = random.randint(1, 20)
         # If there is not enough balance, bid entire balance.
         if self.submitted_bid > self.balance:
@@ -28,13 +29,15 @@ class Car:
 
         # Return a tuple of the car ID and the bid.
         # Car ID is included so that the intersection can keep track of which car submitted which bid.
-        return self.id, self.submitted_bid
+        return self.id, self.submitted_bid, self.waiting_time
 
     def pay_bid(self, price):
         # Price is not necessarily the same as the bid (e.g. 2nd price auction)
         self.balance -= price
+        if self.balance < 0: # This should never happen, we the bid is limited by the balance
+            print("ERROR: Car {} has negative balance".format(self.id))
         print("Car {} paid {} to exit intersection".format(self.id, price))
 
     def set_balance(self, balance):
-        # Set the balance of the car to the given balance
+        # Set the balance of the car to the given balance. Used for the wage distribution.
         self.balance = balance
--- src/car_queue.py
+++ src/car_queue.py
@@ -11,6 +11,7 @@ class CarQueue:
         self.capacity = max_capacity
         self.num_of_cars = len(self.cars)
         self.time_inactive = 0
+        self.bids = {}
 
     def __str__(self):
         # Create list of all IDs of cars in the queue
@@ -18,7 +19,7 @@ class CarQueue:
         for car in self.cars:
             car_ids.append(car.id)
 
-        return f'CarQueue contains cars with IDs: {car_ids}'
+        return f'Car Queue contains cars with IDs: {car_ids}'
 
     def queue_length(self):
         return len(self.cars)
@@ -27,7 +28,7 @@ class CarQueue:
         if self.has_capacity():
             self.cars.append(car)
         else:
-            print("CarQueue is full, cannot add car")
+            print("Car Queue is full, cannot add car")
             return -1
 
     def get_first_car(self):
@@ -41,8 +42,51 @@ class CarQueue:
 
     def collect_bids(self):
         bids = {}
-        # A dictionary is used so that we know which car submitted which bid.
+        # A dictionary is used so that we know which car submitted which bid, as well as the waiting time.
         for car in self.cars:
-            car_id, bid = car.submit_bid()
-            bid[car_id] = bid
+            car_id, bid, waiting_time = car.submit_bid()
+            bid[car_id] = (bid, waiting_time)
         return bids
+
+    def win_auction(self, winning_bid):
+        # This is executed by the car queue that won the auction. The winning bid is given by the mechanism, and can be the 2nd bid for example.
+        # The waiting time is also reset for the cars in this queue.
+
+        # First, the bid must be paid
+        total_amount_paid = 0  # TODO: Remove this if it works DEBUG ONLY!!!
+        queue_car_ids = []  # This holds the IDs of all cars in the queue
+        queue_bids = []  # This holds the bids of all cars in the queue, in the same order as the IDs
+        total_submitted_bid = 0  # This is the sum of the bids of all cars in the queue
+        for bid in self.bids.items:
+            queue_car_ids.append(bid[0])
+            queue_bids.append(bid[1][0])
+            total_submitted_bid += bid[1][0]
+
+        for i in range(len(queue_car_ids)):
+            # The winning bid is divided proportionally depending on the individual bids of the cars in the queue.
+            individual_price = winning_bid * \
+                queue_bids[i] / total_submitted_bid
+            self.cars[i].pay_bid(individual_price)
+            total_amount_paid += individual_price
+
+        print("In total, the cars in the queue paid {} to exit the intersection. The total amount to be paid was: {}".format(
+            total_amount_paid, winning_bid))
+
+        # Second, the waiting time must be reset, as the cars received priority
+        for car in self.cars:
+            car.waiting_time = 0
+            
+        # Third, the bids must be reset, so that the next auction can be run.
+        self.reset_bids()
+
+    def lose_auction(self):
+        # This is executed by the car queues that lost the auction.
+        # First, the waiting time must be increased for all cars in the queue.
+        for car in self.cars:
+            car.waiting_time += 1
+        # Second, the bids must be reset, so that the next auction can be run.
+        self.reset_bids()
+
+    def reset_bids(self):
+        # Reset the bids for the car queue, so that the next auction can be run.
+        self.bids = self.bids.clear()
