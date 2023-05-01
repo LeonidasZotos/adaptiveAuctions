609b0bd6a61460296e2f9766bd2792fe7a1f71aa
LeoZotos <zotos.leo@gmail.com>
2023-05-01 13:59:01 +0200

    fixed argument evaluation implementation

--- aatc.py
+++ aatc.py
@@ -5,6 +5,8 @@ import argparse
 from src import simulate, clean
 
 # Utility Functions
+
+
 def float_range(mini, maxi):
     """Return function handle of an argument type function for 
        ArgumentParser checking a float range: mini <= arg <= maxi
@@ -20,10 +22,10 @@ def float_range(mini, maxi):
         try:
             f = float(arg)
         except ValueError:
-            raise argparse.ArgumentTypeError("must be a floating point number")
+            raise argparse.ArgumentTypeError("Must be a floating point number")
         if f < mini or f > maxi:
             raise argparse.ArgumentTypeError(
-                "must be in range [" + str(mini) + " .. " + str(maxi)+"]")
+                "Must be in range [" + str(mini) + " .. " + str(maxi)+"]")
         return f
 
     # Return function handle to checking function
@@ -47,14 +49,14 @@ if __name__ == '__main__':
     simulate_parser.add_argument(
         "--grid_size",
         default=3,
-        options=range(1, 100),
+        choices=range(1, 100),
         type=int,
         help="Size of the traffic grid. Defaults to 3 (9 intersections). Must be an integer between 1 and 100."
     )
     simulate_parser.add_argument(
         "--queue_capacity",
         default=4,
-        options=range(1, 100),
+        choices=range(1, 100),
         type=int,
         help="Capacity of each car queue. Defaults to 4. Must be an integer between 1 and 100."
     )
@@ -62,16 +64,14 @@ if __name__ == '__main__':
     simulate_parser.add_argument(
         "--congestion_rate",
         default=0.5,
-        options=float_range(0, 1),
-        type=float,
+        type=float_range(0, 1),
         help="Rate of congestion (Percentage of occupied spots, 0-1). Defaults to 0.5. Must be a float between 0 and 1."
     )
 
     simulate_parser.add_argument(
         "--credit_balance",
         default=30,
-        options = float_range(1, 1000),
-        type=float,
+        type=float_range(1, 1000),
         help="Initial & Renewal credit balance for each car. Defaults to 30. Must be a float between 1 and 1000."
     )
 
