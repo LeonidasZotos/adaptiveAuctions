9cb360a20bdd22a631757f4a220c10f32634af62
LeoZotos <zotos.leo@gmail.com>
2023-06-08 21:42:29 +0200

    spsa_static experiment

--- python_batch.sh
+++ python_batch.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --job-name=spsa_and_static
-#SBATCH --mem=32GB
+#SBATCH --mem=64GB
 
 module purge
 module load Python/3.9.6-GCCcore-11.2.0
  
 source $HOME/.envs/cars/bin/activate
  
-python3 aatc.py simulate --num_of_simulations 64 --grid_size 7 --num_of_epochs 1000 --congestion_rate 0.4 --auction_modifier_type spsa
+python3 aatc.py simulate --num_of_simulations 5000 --grid_size 5 --num_of_epochs 100000 --congestion_rate 0.4 --auction_modifier_type spsa
 
-python3 aatc.py simulate --num_of_simulations 64 --grid_size 7 --num_of_epochs 1000 --congestion_rate 0.4 --auction_modifier_type static
+python3 aatc.py simulate --num_of_simulations 5000 --grid_size 5 --num_of_epochs 100000 --congestion_rate 0.4 --auction_modifier_type static
 
 deactivate
