8f19c37329daebc6500e10f8967df64a7e013942
LeoZotos <zotos.leo@gmail.com>
2023-06-20 22:56:50 +0200

    new script

--- python_batch.sh
+++ python_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:05:00
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=128
 #SBATCH --job-name=spsa_and_static
 #SBATCH --mem=64GB
 
@@ -15,17 +15,17 @@ congestion_rate=("0.1" "0.15" "0.2")
 intersection_reward=("time" "time_and_urgency")
 auction_modifier_type=("spsa" "static")
 
-# Loop through variable1
+# Loop through congestion_rate
 for var1 in "${congestion_rate[@]}"
 do
-    # Loop through variable2
+    # Loop through intersection_reward
     for var2 in "${intersection_reward[@]}"
     do
-        # Loop through variable3
+        # Loop through auction_modifier_type
         for var3 in "${auction_modifier_type[@]}"
         do
             # Execute your command with the current parameters
-            command="python3 aatc.py run --num_of_simulations 10 --grid_size 7 --num_of_epochs 50 --congestion_rate=$var1 --intersection_reward=$var2 --auction_modifier_type=$var3"
+            command="python3 aatc.py run --num_of_simulations 500 --grid_size 7 --num_of_epochs 2000 --congestion_rate=$var1 --intersection_reward=$var2 --auction_modifier_type=$var3"
             echo "Executing: $command"
             eval $command
         done
