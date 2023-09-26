936eff29c10dbb9ee3c2868642f4edd8d02c3d51
LeoZotos <zotos.leo@gmail.com>
2023-09-26 16:51:46 +0200

    updated scripy

--- hpc_script_experiments.sh
+++ hpc_script_experiments.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --job-name=discretisation_sweep
+#SBATCH --job-name=demo_experiment
 #SBATCH --mem=32GB
 
 module purge
@@ -11,11 +11,13 @@ module load Python/3.9.6-GCCcore-11.2.0
  
 source $HOME/.envs/cars/bin/activate
 
-eval python3 aatc.py run --num_of_simulations 2000 --num_of_epochs 2000  --results_folder results/simple_bandit --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
+num_of_simulations=2000
+num_of_epochs=2000
+eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $num_of_epochs  --results_folder results/simple_bandit --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
 
-eval python3 aatc.py run --num_of_simulations 2000 --num_of_epochs 2000  --results_folder results/svr --adaptive_auction_update_rule svr --adaptive_auction_action_selection ucb1 --all_cars_bid
+eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $num_of_epochs  --results_folder results/svr --adaptive_auction_update_rule svr --adaptive_auction_action_selection ucb1 --all_cars_bid
 
-eval python3 aatc.py run --num_of_simulations 2000 --num_of_epochs 2000  --results_folder results/random --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection random --all_cars_bid
+eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $num_of_epochs  --results_folder results/random --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection random --all_cars_bid
 
 
 deactivate
