699a52cd5f2f405f412fafd7e5ad6f89ec7118a9
LeoZotos <zotos.leo@gmail.com>
2023-09-28 12:31:20 +0200

    only aggressive bidders and more sims

--- hpc_script_experiments.sh
+++ hpc_script_experiments.sh
@@ -11,19 +11,9 @@ module load Python/3.9.6-GCCcore-11.2.0
 
 source $HOME/.envs/cars/bin/activate
 
-num_of_simulations=250
+num_of_simulations=5000
 # num_of_epochs=3000
-eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 5000  --results_folder results/epochs_5000 --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
-
-eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 6000  --results_folder results/epochs_6000 --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
-
-eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 7000  --results_folder results/epochs_7000 --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
-
-eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 9000  --results_folder results/epochs_9000 --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
-
-eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 10000  --results_folder results/epochs_10000 --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
-
-eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 20000  --results_folder results/epochs_20000 --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
+eval python3 aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs 5000  --results_folder results/epochs_5000_accurate --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection ucb1 --all_cars_bid
 
 deactivate
 
