2f3c9586f98b64feb9b4aaf5a41cbeaaca895003
LeoZotos <zotos.leo@gmail.com>
2023-10-09 14:07:07 +0200

    small path in analyses.ipynb and updated script for more sims

--- scripts/hpc_adaptiveVSrandom_script.sh
+++ scripts/hpc_adaptiveVSrandom_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=04:00:00
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --job-name=adaptive_vs_random
-#SBATCH --mem=128GB
+#SBATCH --job-name=adaptive_vs_random2
+#SBATCH --mem=256GB
 
 module purge
 module load Python/3.9.6-GCCcore-11.2.0
@@ -12,7 +12,7 @@ module load Python/3.9.6-GCCcore-11.2.0
 source $HOME/.envs/cars/bin/activate
 
 epochs=10000
-num_of_simulations=1000
+num_of_simulations=10000
 
 # Execute your command with the current parameters
 command="python3 ../aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $epochs --all_cars_bid --with_hotspots --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection boltzmann"
