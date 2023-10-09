5651aa1cba74a31ecb53993001b682d81c7355b4
LeoZotos <zotos.leo@gmail.com>
2023-10-09 20:15:49 +0200

    added zero action selection in script

--- scripts/hpc_adaptiveVSrandom_script.sh
+++ scripts/hpc_adaptiveVSrandom_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128
-#SBATCH --job-name=adaptive_vs_random2
-#SBATCH --mem=256GB
+#SBATCH --job-name=adaptive_vs_random_vs_zero
+#SBATCH --mem=512GB
 
 module purge
 module load Python/3.9.6-GCCcore-11.2.0
@@ -20,6 +20,10 @@ echo "Executing: $command"
 eval $command
 
 command="python3 ../aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $epochs --all_cars_bid --with_hotspots --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection random"
+echo "Executing: $command"
+eval $command
+
+command="python3 ../aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $epochs --all_cars_bid --with_hotspots --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection zero"
 echo "Executing: $command"
 eval $command
 
