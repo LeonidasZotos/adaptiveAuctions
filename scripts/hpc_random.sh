19214e196e8d2e980841d0173e44c5a7c23ded29
LeoZotos <zotos.leo@gmail.com>
2023-10-10 16:21:36 +0200

    split into 3

---
+++ scripts/hpc_random.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --job-name=adaptive
+#SBATCH --mem=128GB
+
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+ 
+source $HOME/.envs/cars/bin/activate
+
+epochs=10000
+num_of_simulations=10000
+
+command="python3 ../aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $epochs --all_cars_bid --with_hotspots --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection random"
+echo "Executing: $command"
+eval $command
+
+
+deactivate
