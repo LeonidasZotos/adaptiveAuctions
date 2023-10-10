737901dc54da44e68ed1f7f80fd2d98749830262
LeoZotos <zotos.leo@gmail.com>
2023-10-10 17:57:09 +0200

    adapted reqs

--- scripts/hpc_zero.sh
+++ scripts/hpc_zero.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --job-name=zero
-#SBATCH --mem=128GB
+#SBATCH --mem=256GB
 
 module purge
 module load Python/3.9.6-GCCcore-11.2.0
@@ -12,7 +12,7 @@ module load Python/3.9.6-GCCcore-11.2.0
 source $HOME/.envs/cars/bin/activate
 
 epochs=10000
-num_of_simulations=10000
+num_of_simulations=5000
 
 command="python3 ../aatc.py run --num_of_simulations $num_of_simulations --num_of_epochs $epochs --all_cars_bid --with_hotspots --adaptive_auction_update_rule simple_bandit --adaptive_auction_action_selection zero"
 echo "Executing: $command"
